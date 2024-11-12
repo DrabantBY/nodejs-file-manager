bb3d817b255004d9ae8b7e404690752df7865c1b
DrabantBY <drabant2022@gmail.com>
2024-11-12 18:47:21 +0300

    refactor: add promise in copyFile function

--- src/utils/copyFile.js
+++ src/utils/copyFile.js
@@ -1,17 +1,17 @@
 import { createReadStream, createWriteStream } from 'node:fs';
-import handleError from '../handlers/handleError.js';
 import handleTarget from '../handlers/handleTarget.js';
 
-const copyFile = (args) => {
-	const [source, target] = handleTarget(args);
+const copyFile = (args) =>
+	new Promise((resolve, reject) => {
+		const [source, target] = handleTarget(args);
 
-	const readStream = createReadStream(source);
-	readStream.on('error', handleError);
+		const readStream = createReadStream(source);
+		readStream.on('error', reject);
 
-	const writeStream = createWriteStream(target);
-	writeStream.on('error', handleError);
+		const writeStream = createWriteStream(target, { flags: 'wx' });
+		writeStream.on('error', reject);
 
-	readStream.pipe(writeStream);
-};
+		readStream.pipe(writeStream).on('finish', resolve);
+	});
 
 export default copyFile;
